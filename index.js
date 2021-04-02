4d21d278eb42112a560fe7eedd10795803173868
sahidularif <sahidularif1@gmail.com>
2021-04-02 15:42:56 +0600

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 require('dotenv').config();
 
 app.use(cors());
@@ -21,15 +22,34 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
     const productCollection = client.db("cosmetics").collection("products");
-    console.log(uri)
-    console.log(err)
+    const orderCollection = client.db("cosmetics").collection("orders");
 
     app.get('/products', (req, res) => {
         productCollection.find()
-        .toArray((err, items) => {
-            res.send(items)
-        })
-    })
+            .toArray((err, items) => {
+                res.send(items)
+            })
+    });
+
+    app.get('/checkout/:_id', (req, res) => {
+        console.log('ok')
+        productCollection
+            .find({ _id: ObjectId(req.params._id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            });
+    });
+
+
+
+    app.get('/userOrders', (req, res) => {
+        // console.log(req.query.email)
+        orderCollection
+            .find({ userEmail: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);
+            });
+    });
 
 
     app.post('/addProduct', (req, res) => {
@@ -41,10 +61,27 @@ client.connect(err => {
                 res.send(result.insertedCount > 0)
             })
     })
+
+    app.post('/addOrder', (req, res) => {
+        const newOrder = req.body;
+        orderCollection.insertOne(newOrder).then((result) => {
+            res.send(result.insertedCount > 0);
+        });
+    });
+
+    // Delete product order from mongoDB 
+    app.delete('/deleteOrder/:_id', (req, res) => {
+        orderCollection
+            .deleteOne({ _id: ObjectId(req.params._id) })
+            .then((result) => {
+                res.send(result.deletedCount > 0);
+            });
+    });
+
 });
 
 
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`http://localhost:${port}`)
 })
